446f854c76189ac165ee5742fe71be76bc3757ff
“wahyupra” <wahyupraunsri@gmail.com>
2024-08-27 09:51:58 +0700

    Hello First Commit Here!

---
+++ 01/Aug26/javascript/day-01.js
@@ -0,0 +1,43 @@
+console.log("Hello World!");
+console.log("Hello Purwadhika!");
+console.log("Let's learn javascript");
+
+//how to quokka
+//ctrl+k -> q
+
+//how to multiple line comment
+// ctrl+k+c
+
+// variabel case sensitive
+// owo
+//variable
+
+
+let namaDepan;
+namaDepan = "wahyu";
+let namaBelakang = "pratama";
+console.log(namaDepan);
+console.log(namaBelakang);
+console.log(namaDepan+" "+namaBelakang);
+
+namaBelakang = "saya";
+console.log(namaBelakang)
+var buah = "jeruk"; //absoulete variable meniban value yang lama
+let pakaian = "celana"; // update data variable tanpa menimpa variable
+const motor = "suzuki"; // data tidak bisa dirubah (constant variable)
+const space =" ";
+console.log(buah+space+pakaian+space+motor+namaBelakang);
+
+//Variable Date Type
+//Primitive (String, Number & Digit, Boolean, Null, and Undifine)
+let angka = 2;
+console.log(typeof angka);
+angka = "2";
+console.log(typeof angka); 
+let Undifine;
+console.log(typeof Undifine);
+let Null = null;
+console.log(typeof Null);
+let Boolean = false;
+console.log(typeof Boolean);
+//Non Primitive (Array and Object)

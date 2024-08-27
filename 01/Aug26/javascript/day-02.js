843a396d26703b960f64ffe9d5d23ad91441e244
“wahyupra” <wahyupraunsri@gmail.com>
2024-08-27 19:22:00 +0700

    day 02

---
+++ 01/Aug26/javascript/day-02.js
@@ -0,0 +1,13 @@
+//Variable Date Type
+//Primitive (String, Number & Digit, Boolean, Null, and Undifine)
+let angka = 2;
+console.log(typeof angka);
+angka = "2";
+console.log(typeof angka); 
+let Undifine;
+console.log(typeof Undifine);
+let Null = null;
+console.log(typeof Null);
+let Boolean = false;
+console.log(typeof Boolean);
+//Non Primitive (Array and Object)
